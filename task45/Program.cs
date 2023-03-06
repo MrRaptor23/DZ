7b62a96bee1e06a0bb81dd9c519e221b8d64ce66
MrRaptor23 <123752881+MrRaptor23@users.noreply.github.com>
2023-03-06 16:13:34 +0300

    Add files via upload

---
+++ task45/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования
+// void RandArray (int[] array)
+// {
+// Random rand = new Random();
+// for (int i = 0; i < arr.Length; i++) 
+//             copy[i] = arr[i];
+//             {
+// array [i] = rand.Next(1,11);
+// }
+// }
+// void PrintArr (int [] array)
+// console.Write('[')
+using System;
+ 
+public class Example
+{
+    public static void Main()
+    {
+        int[] arr = { 5, 4, 7, 2, 9 };
+ 
+        int[] copy = new int[arr.Length];
+        for (int i = 0; i < arr.Length; i++) {
+            copy[i] = arr[i];
+        }
+ 
+        Console.WriteLine(String.Join(", ", copy));       
+    }
+}
